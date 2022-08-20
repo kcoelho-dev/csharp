f104e69014dd0ee188ff5e4f3e36893c2353e014
Kelvin Coelho Loiola <kcoelho.dev@gmail.com>
2022-08-20 09:07:51 -0300

    Moving 'MoviesConsoleAPI' into csharp repository

---
+++ Projects/MoviesConsoleAPI/MoviesConsoleAPI/Models/MovieInfo.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+
+namespace MoviesConsoleAPI.Models
+{
+    /// <summary> <c>MovieInfo</c> A Class that contains the most relevant information to solve the problem </summary>
+    /// obs: As we don't need the Title and imdbID to reach the solution, these attributes are not used.
+    public class MovieInfo
+    {
+        
+        //public string? Title { get; set; }
+
+        /// <summary> <c>Year</c> The year that the movie was released </summary>
+        [JsonProperty("Year")]
+        public int Year { get; set; }
+
+        //public string imdbID { get; set; }
+
+    }
+}
---
+++ Projects/MoviesConsoleAPI/MoviesConsoleAPI/Models/PageInfo.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace MoviesConsoleAPI.Models
+{
+    /// <summary> <c>PageInfo</c> A class to store basic informations of the reached page.
+    /// e.g.: current page, total of results, total of pages containing results, total of pages
+    /// and the data containing movies information
+    /// </summary>
+    public class PageInfo
+    {
+        /// <summary> <c>PageInfo(int page)</c> Constructor to initialize the an instance of the class at the first page</summary>
+        public PageInfo(int page) { Page = page; }
+
+        [JsonProperty("page")]
+        /// <summary> <c>Page</c> Current page to reach the API results </summary>
+        public int Page { get; set; }
+
+        /// <summary> <c>Total</c> Total of results based on the search title </summary>
+        [JsonProperty("total")]
+        public int Total { get; set; }
+        
+        /// <summary> <c>TotalPages</c> The total of pages than contains results </summary>
+        [JsonProperty("total_pages")]
+        public int TotalPages { get; set; }
+
+        /// <summary> <c>Data</c> The data to be stored at MovieInfo class <summary>
+        public IEnumerable<MovieInfo> Data { get; set; }
+    }
+}
